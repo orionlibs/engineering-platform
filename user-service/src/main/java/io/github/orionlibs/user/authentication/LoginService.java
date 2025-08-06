c2b9ec2009a0fbecfeeecd7d881322550e01408a
Dimitrios Efthymiou <efthymiou.dimitrios1@gmail.com>
2025-08-06 12:29:22 +0100

    2

--- user-service/src/main/java/io/github/orionlibs/user/authentication/LoginService.java
+++ user-service/src/main/java/io/github/orionlibs/user/authentication/LoginService.java
@@ -4,13 +4,13 @@ import io.github.orionlibs.core.data.ResourceNotFoundException;
 import io.github.orionlibs.core.event.Publishable;
 import io.github.orionlibs.core.jwt.JWTService;
 import io.github.orionlibs.core.user.UserService;
+import io.github.orionlibs.core.user.model.OrionUserDetails;
 import io.github.orionlibs.user.authentication.api.LoginRequest;
 import io.github.orionlibs.user.event.EventUserLoggedIn;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
-import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
@@ -30,7 +30,7 @@ public class LoginService implements Publishable
         Authentication auth = new UsernamePasswordAuthenticationToken(requestBean.getUsername(), requestBean.getPassword());
         try
         {
-            UserDetails user = userService.loadUserByUsername(requestBean.getUsername());
+            OrionUserDetails user = userService.loadUserByUsername(requestBean.getUsername());
             authenticationManager.authenticate(auth);
             String token = jwtService.generateToken((String)auth.getPrincipal(), user.getAuthorities());
             publish(EventUserLoggedIn.EVENT_NAME, EventUserLoggedIn.builder()
