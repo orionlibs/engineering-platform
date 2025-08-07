4939ab1f0b9c16368d410a53d0966a29179bad38
Dimitrios Efthymiou <efthymiou.dimitrios1@gmail.com>
2025-08-07 12:20:14 +0100

    1

--- core/src/main/java/io/github/orionlibs/core/api/key/model/ApiKeyDAO.java
+++ core/src/main/java/io/github/orionlibs/core/api/key/model/ApiKeyDAO.java
@@ -1,4 +1,4 @@
-package io.github.orionlibs.core.api.model;
+package io.github.orionlibs.core.api.key.model;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
--- core/src/main/java/io/github/orionlibs/core/api/key/model/ApiKeyModel.java
+++ core/src/main/java/io/github/orionlibs/core/api/key/model/ApiKeyModel.java
@@ -1,4 +1,4 @@
-package io.github.orionlibs.core.api.model;
+package io.github.orionlibs.core.api.key.model;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
