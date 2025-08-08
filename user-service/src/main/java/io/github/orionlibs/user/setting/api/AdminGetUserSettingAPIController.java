443041de04ea995dd12d3abafb8c82bf1d852d1c
Dimitrios Efthymiou <efthymiou.dimitrios1@gmail.com>
2025-08-08 15:18:22 +0100

    1

---
+++ user-service/src/main/java/io/github/orionlibs/user/setting/api/AdminGetUserSettingAPIController.java
@@ -0,0 +1,58 @@
+package io.github.orionlibs.user.setting.api;
+
+import io.github.orionlibs.core.user.setting.model.UserSettingsModel;
+import io.github.orionlibs.user.ControllerUtils;
+import io.github.orionlibs.user.setting.UserSettingsService;
+import io.github.orionlibs.user.setting.converter.UserSettingModelToDTOConverter;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.enums.ParameterIn;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import java.util.Optional;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(ControllerUtils.baseAPIPath)
+@Tag(name = "Users", description = "User manager")
+public class AdminGetUserSettingAPIController
+{
+    @Autowired
+    private UserSettingsService userSettingsService;
+    @Autowired
+    private UserSettingModelToDTOConverter userSettingModelToDTOConverter;
+
+
+    @Operation(
+                    summary = "Get user setting",
+                    description = "Get user setting",
+                    parameters = {@io.swagger.v3.oas.annotations.Parameter(
+                                    name = "userID",
+                                    description = "The userID of the account of which to retrieve settings",
+                                    required = true,
+                                    in = ParameterIn.PATH,
+                                    schema = @Schema(type = "string")
+                    ), @io.swagger.v3.oas.annotations.Parameter(
+                                    name = "settingsID",
+                                    description = "The settingsID to retrieve",
+                                    required = true,
+                                    in = ParameterIn.PATH,
+                                    schema = @Schema(type = "string")
+                    )},
+                    responses = {@ApiResponse(responseCode = "200", description = "User account setting returned")}
+    )
+    @GetMapping(value = "/users/{userID}/settings/{settingsID}", consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
+    @PreAuthorize("hasAuthority('ADMINISTRATOR')")
+    public ResponseEntity<UserSettingDTO> adminGetUserSetting(@PathVariable String userID, @PathVariable String settingsID)
+    {
+        Optional<UserSettingsModel> userSetting = userSettingsService.getByIDAndUserID(settingsID, userID);
+        return ResponseEntity.ok(userSettingModelToDTOConverter.convert(userSetting));
+    }
+}
---
+++ user-service/src/test/java/io/github/orionlibs/user/setting/api/AdminGetUserSettingAPIServiceTest.java
@@ -0,0 +1,75 @@
+package io.github.orionlibs.user.setting.api;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import io.github.orionlibs.core.tests.APITestUtils;
+import io.github.orionlibs.core.user.model.UserDAO;
+import io.github.orionlibs.core.user.model.UserModel;
+import io.github.orionlibs.core.user.setting.model.UserSettingsModel;
+import io.github.orionlibs.user.ControllerUtils;
+import io.github.orionlibs.user.registration.UserRegistrationService;
+import io.github.orionlibs.user.registration.api.UserRegistrationRequest;
+import io.github.orionlibs.user.setting.UserSettingsService;
+import io.restassured.RestAssured;
+import io.restassured.response.Response;
+import java.util.List;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.server.LocalServerPort;
+import org.springframework.http.HttpHeaders;
+import org.springframework.test.context.ActiveProfiles;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@ActiveProfiles("test")
+public class AdminGetUserSettingAPIServiceTest
+{
+    @LocalServerPort int port;
+    @Autowired UserDAO dao;
+    @Autowired UserRegistrationService userRegistrationService;
+    @Autowired UserSettingsService userSettingsService;
+    @Autowired APITestUtils apiUtils;
+    String basePath;
+    HttpHeaders headers;
+    UserModel user;
+    UserModel admin;
+
+
+    @BeforeEach
+    void setup()
+    {
+        basePath = "http://localhost:" + port + ControllerUtils.baseAPIPath + "/users/%s/settings/%s";
+        dao.deleteAll();
+        headers = new HttpHeaders();
+        user = userRegistrationService.registerUser(UserRegistrationRequest.builder()
+                        .username("me@email.com")
+                        .password("bunkzh3Z!")
+                        .authority("USER")
+                        .firstName("Dimi")
+                        .lastName("Emilson")
+                        .phoneNumber("07896620211")
+                        .build());
+        admin = userRegistrationService.registerUser(UserRegistrationRequest.builder()
+                        .username("admin@email.com")
+                        .password("bunkzh3Z!")
+                        .authority("ADMINISTRATOR")
+                        .firstName("Dimi")
+                        .lastName("Emilson")
+                        .phoneNumber("07896620211")
+                        .build());
+    }
+
+
+    @Test
+    void adminGetUserSetting()
+    {
+        List<UserSettingsModel> settings = userSettingsService.getByUserID(user.getId().toString());
+        RestAssured.baseURI = String.format(basePath, user.getId().toString(), settings.get(0).getId().toString());
+        Response response = apiUtils.makeGetAPICall(headers, admin.getId().toString(), "ADMINISTRATOR");
+        assertThat(response.statusCode()).isEqualTo(200);
+        UserSettingDTO body = response.as(UserSettingDTO.class);
+        assertThat(body.settingName()).isEqualTo("theme");
+        assertThat(body.settingValue()).isEqualTo("dark");
+    }
+}
